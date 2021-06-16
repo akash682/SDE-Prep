cea71e71ea3da7fa4ec23e5ef5500151b56b79ea
Akash Lohani <akash.lohani@mavs.uta.edu>
2021-06-15 22:27:52 -0700

    2021/6/15 Commit v1

---
+++ LectureNote/Graph.py
@@ -0,0 +1,19 @@
+"""
+Graph: Good to express real world relationship
+
+Node: Node(Vertex), Edge
+i.e. Network, Friendships, Roads
+
+Directed/Undirected
+Directed: describes the information of the flow relationship(i.e One way Road, Twitter)
+Undirected: no info about direction(i.e. Facebook)
+
+Weighted/Unweighted
+Information on the Edges! 
+i.e. Go to the destination in more efficient way, to find the shorter path
+
+Cyclic/Acyclic
+When Nodes are connected in circular fashion
+Google Maps
+
+"""
