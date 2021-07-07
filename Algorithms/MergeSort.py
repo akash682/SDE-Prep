d6eceff72236a0542ae41f9a3af351a3076a5734
Akash Lohani <akash.lohani@mavs.uta.edu>
2021-07-06 21:08:59 -0700

    2021/7/6 Commit v1

---
+++ Algorithms/MergeSort.py
@@ -0,0 +1,8 @@
+"""
+Divide & Conquer 
+This will allow us to make the sorting faster!!
+Divide into part and merge it one by one!
+
+Stable: If we have the same value say 6,6 it doesn't change the order
+"""
+
