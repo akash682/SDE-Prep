2101fa93e51361c6ce5a8a34daaae2466ffc0a1c
Akash Lohani <akash.lohani@mavs.uta.edu>
2021-07-06 22:13:21 -0700

    2021/7/6 Commit v3

---
+++ Algorithms/Sorting/SortingSelectionQuiz.py
@@ -0,0 +1,40 @@
+"""
+//#1 - Sort 10 schools around your house by distance:
+Quick Sort
+Ans: Insertion Sort --> Good for small set!
+
+//#2 - eBay sorts listings by the current Bid amount:
+Quick Sort
+Ans: Radix or Counting Sort(It's always number)
+
+//#3 - Sport scores on ESPN
+Quick Sort
+Ans: Quick Sort (Different variety of number set)
+
+//#4 - Massive database (can't fit all into memory) needs to sort through past year's user data
+Quick Sort
+Ans: Merge Sort(Because the database is massive we need to ensure it doesn't go worst case)
+
+//#5 - Almost sorted Udemy review data needs to update and add 2 new reviews
+Insertion Sort
+Ans: Insertion Sort(Even the data is huge, insertion sort works really good insertion for preordered list )
+
+//#6 - Temperature Records for the past 50 years in Canada
+Quick Sort
+Ans: Radix Counting Sort(Decimal cannot do in Radix or counting so if that's the case QUickSort)
+
+//#7 - Large user name database needs to be sorted. Data is very random.
+Quick Sort
+Ans: Merge Sort(Memory is not costly), Quick Sort
+
+//#8 - You want to teach sorting for the first time
+Bubble Sort, Selection Sort
+
+Factor
+1. Is it a number?
+2. Is it almost sorted?
+3. Is the memory costly?
+4. Is the data massive?
+5. Is it decimal?
+6. Is it ordered?
+"""
