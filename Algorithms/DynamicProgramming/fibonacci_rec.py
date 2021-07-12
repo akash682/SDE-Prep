1f631f3803fcc4d3c872bd03466364f721fa6f26
Akash Lohani <akash.lohani@mavs.uta.edu>
2021-07-11 22:48:38 -0700

    2021/7/11 Commitv3

--- Algorithms/DynamicProgramming/fibonacci_rec.py
+++ Algorithms/DynamicProgramming/fibonacci_rec.py
@@ -1,6 +1,8 @@
+
+"""
+# My Solution
 val_store = {}
 calculation = 0
-
 def fib(n):
     if n == 1:
         return 1
@@ -10,17 +12,24 @@ def fib(n):
     if n-2 in val_store.keys():
         fib_n2 = val_store[n-2]
     else:
-        print(n-2)
         fib_n2 = fib(n-2)
         val_store[n-2] = fib_n2
     
     if n-1 in val_store.keys():
         fib_n1 = val_store[n-1]
     else:
-        print(n-1)
         fib_n1 = fib(n-1)
         val_store[n-1] = fib_n1
 
     return fib_n1 + fib_n2
 
+"""
+val_store = {}
+def fib(n):
+    if n < 2:
+        return n
+    if n in val_store.keys():
+        return val_store[n]
+    
+    return fib(n-1) + fib(n-2)
 print(fib(7))
