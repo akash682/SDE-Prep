6c6987cc2b6dd18fb4eed2e93f047653304ee51d
Akash Lohani <akash.lohani@mavs.uta.edu>
2021-07-11 22:46:17 -0700

    2021/7/11 Commitv2

---
+++ Algorithms/DynamicProgramming/fibonacci_rec.py
@@ -0,0 +1,26 @@
+val_store = {}
+calculation = 0
+
+def fib(n):
+    if n == 1:
+        return 1
+    if n == 0:
+        return 0
+    
+    if n-2 in val_store.keys():
+        fib_n2 = val_store[n-2]
+    else:
+        print(n-2)
+        fib_n2 = fib(n-2)
+        val_store[n-2] = fib_n2
+    
+    if n-1 in val_store.keys():
+        fib_n1 = val_store[n-1]
+    else:
+        print(n-1)
+        fib_n1 = fib(n-1)
+        val_store[n-1] = fib_n1
+
+    return fib_n1 + fib_n2
+
+print(fib(7))
