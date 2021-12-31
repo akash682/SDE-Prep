62e4df6f7d1b5aaadba6dffbfeed4559a9992945
Akash Lohani <allohani@amazon.com>
2021-12-31 13:11:36 -0800

    20211231v3

--- LectureNote/New/Section6/array.py
+++ LectureNote/New/Section6/array.py
@@ -16,6 +16,17 @@ class Array:
         del self.data[self.length-1]
         self.length -=1
         return lastItem
+    
+    def delete(self, index):
+        item = self.data[index]
+        self.shiftItems(index)
+    
+    def shiftItems(self, index):
+        for i in range(index, self.length-1):
+            self.data[i] = self.data[i+1];
+        del self.data[self.length-1]
+        self.length -= 1
+
 
 # Define
 newArray = Array()
@@ -26,8 +37,14 @@ newArray = Array()
 # Push
 newArray.push('hi')
 newArray.push('hello')
+newArray.push('third')
+newArray.push('Forth')
 print(newArray.data, newArray.length)
 
 # Pop
 newArray.pop()
+print(newArray.data, newArray.length)
+
+# Delete
+newArray.delete(1)
 print(newArray.data, newArray.length)
