b9e3d8000785b103bd6102a06398fe0d1b8901fb
Akash Lohani <akash.lohani@mavs.uta.edu>
2022-01-17 00:17:00 -0800

    recursivev3

--- LectureNote/New/Section12/note.py
+++ LectureNote/New/Section12/note.py
@@ -11,9 +11,29 @@ which means we might encounter some cases that the call the function in the func
 # Identify the recursive case
 # Get closer and closer and return when needed. Ususally you have 2 returns
 
+## Recursive vs Iterative
+Anything you do with a recursion Can be done iteratively(loop)
+Some problem can be described in easy coding 
+It makes the code dry, do not repeat the code itself
+less loops happening, it's not always a good decision
+
+++ PLUS ++
+DRY
+Readablility
+
+-- MINUS --
+Large Stack
+
+Trees: BFS + DFS (Recursion is preffered)
+Everytime you are using a tree or converting something into a tree, consider recursion
+1. Divided into a number of subproblems that are smaller instances of the same problem.
+2. Each instance of the subproblem is identical in nature
+3. The solutions of each subproblem can be combined to solve the problem at hand.
+
+Divide and Concur using Recursion
+
 """
-
-
+print("hello")
 
 def inception(counter):
     if counter > 3:
